d8b2df923cf678778146cd921c6e71c289c1946d
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-14 23:18:12 +0700

    rename components desktop folder name

--- src/app/(desktop)/layout.tsx
+++ src/app/(desktop)/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import NavigationBar from "@/components/common/desktop/NavigationBar";
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+import NavigationBar from "@/components/common/main/NavigationBar";
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
