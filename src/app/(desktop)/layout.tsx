484c1e501cdbd53d2db6c4eda3235c0daba78647
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-17 21:32:29 +0700

    add tutorial

--- src/app/(desktop)/layout.tsx
+++ src/app/(desktop)/layout.tsx
@@ -2,6 +2,8 @@ import "./../globals.css";
 import NavigationBar from "@/components/common/main/NavigationBar";
 import Dock from "@/components/common/main/Dock";
 import WindowRenderer from "@/components/common/main/WindowRenderer";
+import InteractiveTutorial from "@/components/common/main/InteractiveTutorial";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout({
   children,
@@ -14,6 +16,8 @@ export default function RootLayout({
       {children}
       <WindowRenderer />
       <Dock />
+      <InteractiveTutorial />
+      <Toaster richColors position="top-center" />
     </>
   );
 }
